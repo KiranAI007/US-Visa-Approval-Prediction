4fcfabe8946030844be5886515e12d031fc94f86
KiranAI007 <kiranrkrishnan07@gmail.com>
2024-06-14 20:24:37 +0530

    updates in notebook

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # US-Visa-Approval-Prediction
 To predict the eligibility of US Visa 
+
+# How to run ?
+'''
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='US-Visa-Approval-Prediction',
+    version='1.0.0',
+    author='Kiran R Krishnan',
+    author_email='kiranrkrishnan07@gmail.com',
+    packages=find_packages()
+)
